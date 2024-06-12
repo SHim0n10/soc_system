31c846380e4c6777cc0a58aa20224956fbb67813
SHim0n10 <49526116+SHim0n10@users.noreply.github.com>
2024-06-12 16:49:30 +0200

    filtrovanie

--- soc/urls.py
+++ soc/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('ucitel/<ucitel>', views.vypis_ucitela, name='ucitel'),
     path('student/<student>', views.vypis_studenta, name='student'),
     path('tema/<tema>', views.vypis_temu, name='tema'),
+    path('filter_temy', views.filter_temy, name='filter_temy'),
 
 
 ]
--- soc/views.py
+++ soc/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
 from . models import *
 
 def vypis_tem(request):
     temy = Tema.objects.all().order_by("dostupnost", "id")
+    konzultanti = Ucitel.objects.all().order_by("priezvisko")
+    studenti = Student.objects.all().order_by("priezvisko")
+    odbory = Odbor.objects.all().order_by("nazov")
 
-    return render(request, "soc/index.html", {"temy":temy})
+    return render(request, "soc/index.html", {"temy":temy, "konzultanti":konzultanti, "studenti":studenti, "odbory":odbory})
 
 def vypis_ucitela(request, ucitel):
     ucitel = Ucitel.objects.get(pk=ucitel)
@@ -25,3 +29,41 @@ def vypis_temu(request, tema):
         tema = False
 
     return render(request, "soc/index.html", {"tema":tema})
+
+def filter_temy(request):
+    nazov_query = request.GET.get('nazov', '')
+    popis_query = request.GET.get('popis', '')
+    konzultant_query = request.GET.get('konzultant', '')
+    student_query = request.GET.get('student', '')
+    odbor_query = request.GET.get('odbor', '')
+    konzultacie_query = request.GET.get('konzultacie', '')
+
+    temy = Tema.objects.all()
+
+    if nazov_query:
+        temy = temy.filter(nazov__icontains=nazov_query)
+    if popis_query:
+        temy = temy.filter(popis__icontains=popis_query)
+    if konzultant_query:
+        temy = temy.filter(konzultant=konzultant_query)
+    if student_query:
+        temy = temy.filter(student=student_query)
+    if odbor_query:
+        temy = temy.filter(odbor=odbor_query)
+    if konzultacie_query:
+        temy = temy.filter(konzultacie__icontains=konzultacie_query)
+
+    results = []
+    for tema in temy:
+        results.append({
+            'id': tema.id,
+            'nazov': tema.nazov,
+            'popis': tema.popis,
+            'konzultant': str(tema.konzultant),  # Convert to string
+            'dostupnost': tema.dostupnost.nazov,
+            'student': str(tema.student) if tema.student else '----',  # Convert to string
+            'odbor': str(tema.odbor),
+            'konzultacie': tema.konzultacie
+        })
+    return JsonResponse(results, safe=False)
+
